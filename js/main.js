547f47235f7c4b25d25d8569369522d9d69ca9f2
abbosov123 <abbosov1122@yandex.com>
2022-12-02 08:56:38 +0500

    Create quality section with repsonsive design

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ var elBtn = document.querySelector(".header__btn");
 elBtn.addEventListener("click", function () {
     elHeader.classList.toggle("header--open");
 });
+
+
+var karobka = document.querySelector(".modal");
+var ochishuchunknopka = document.querySelector(".modal__open");
+var elBtnClose = document.querySelector(".modal__close");
+
+
+ochishuchunknopka.addEventListener("click", function () {
+    karobka.classList.add("modal--open");
+});
+
+elBtnClose.addEventListener("click", function () {
+    karobka.classList.remove("modal--open");
+});
